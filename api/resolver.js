eb08026290637ffa12858e644e08526cbe74be07
jonathantzh <jtan202@e.ntu.edu.sg>
2017-01-21 14:22:45 -0500

    initial commit

---
+++ api/resolver.js
@@ -0,0 +1,14 @@
+module.exports = function(req, res) {
+  var data = JSON.parse(req.body.params);
+  if (!data) {
+    res.status(403 /* Unauthorized */ ).send('Invalid params');
+    return;
+  }
+
+  var width = data.width > 600 ? 600 : data.width;
+  var html = '<img style="max-width:100%;" src="' + data.src + '" width="' + width + '"/>';
+  res.json({
+    body: html
+    // Add raw:true if you're returning content that you want the user to be able to edit
+  });
+};
